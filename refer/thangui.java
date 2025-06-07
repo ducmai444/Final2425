d9852a886cba2a4fb0ce57ba3a58d9fefdd39331
Mai Ba Duc <109147966+ducmai444@users.noreply.github.com>
2025-06-07 16:16:55 +0700

    Update thangui.java

--- refer/thangui.java
+++ refer/thangui.java
@@ -2,7 +2,7 @@ package refer;
 
 public class thangui {
     public static void main(String[] args) {
-        System.out.println("Kính gửi các bạn,");
+        System.out.println("Kính gửi bro1,");
         System.out.println();
         System.out.println("Tôi viết thư này để gửi lời chào thân ái đến tất cả mọi người.");
         System.out.println("Chúc các bạn một ngày tốt lành và tràn đầy niềm vui.");
