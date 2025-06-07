666616da80e9aeb7100d8a34aad0495a3261e6a0
Hoaibao <ng.hoaibao03ht@gmail.com>
2025-06-07 15:55:26 +0700

    Add thangui class

---
+++ refer/thangui.java
@@ -0,0 +1,13 @@
+package refer;
+
+public class thangui {
+    public static void main(String[] args) {
+        System.out.println("Kính gửi các bạn,");
+        System.out.println();
+        System.out.println("Tôi viết thư này để gửi lời chào thân ái đến tất cả mọi người.");
+        System.out.println("Chúc các bạn một ngày tốt lành và tràn đầy niềm vui.");
+        System.out.println();
+        System.out.println("Trân trọng,");
+        System.out.println("Người gửi");
+    }
+}
